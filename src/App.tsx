e6b28b3711278eb6dd72a98ad159f470cc34584e
VladTca <votsats@gmail.com>
2024-07-04 23:14:42 +0200

    Refactored App component to use useState for value instead of count, added handlers for increment, decrement, setting to localStorage, getting from localStorage, and clearing localStorage. Updated JSX to display value and buttons for each handler.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+    const [value, setValue] = useState<number>(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+    const decHandler = () => {
+        setValue(value - 1)
+    }
+
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue+1', JSON.stringify(value+1))
+    }
+
+    const getFromLocalStorageHandler = () => {
+        const value = localStorage.getItem('counterValue')
+        if (value) {
+            setValue(JSON.parse(value))
+        }
+    }
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+          <h1>{value}</h1>
+          <button onClick={incHandler}>+</button>
+          <button onClick={decHandler}>-</button>
+          <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+          <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+          <button onClick={clearLocalStorageHandler}>clerarLocalStorage</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
