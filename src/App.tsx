777aee0a9bd5bdb359666ebe6504456afe336021
VladTca <votsats@gmail.com>
2024-07-05 00:07:41 +0200

    Refactor App.tsx to include useEffect for setting counter value to localStorage and add TODO for setToSessionStorage function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-
 import './App.css'
 import {useEffect, useState} from "react";
 
 function App() {
 
     const [value, setValue] = useState<number>(0)
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-    useEffect(()=>{
-     localStorage.setItem('counterValue', JSON.stringify(value))
-    },[value])
+    useEffect(() => {
+        const valueAsString = localStorage.getItem('counterValue')
+        console.log('Loaded value from localStorage:', valueAsString)
+        if (valueAsString) {
+            setValue(JSON.parse(valueAsString))
+        }
+        setIsLoaded(true)
+    }, [])
+
+    useEffect(() => {
+        if (isLoaded) {
+            console.log('Saving value to localStorage:', value)
+            localStorage.setItem('counterValue', JSON.stringify(value))
+        }
+    }, [value, isLoaded])
 
     const incHandler = () => {
         setValue(value + 1)
@@ -20,14 +32,14 @@ function App() {
 // TODO: add setToSessionStorage
     const setToLocalStorageHandler = () => {
         localStorage.setItem('counterValue', JSON.stringify(value))
-        localStorage.setItem('counterValue+1', JSON.stringify(value+1))
+        localStorage.setItem('counterValue+1', JSON.stringify(value + 1))
         sessionStorage.setItem('counterValue', JSON.stringify(value))
     }
 
     const getFromLocalStorageHandler = () => {
-        const value = localStorage.getItem('counterValue')
-        if (value) {
-            setValue(JSON.parse(value))
+       const valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            setValue(JSON.parse(valueAsString))
         }
     }
     const clearLocalStorageHandler = () => {
@@ -38,17 +50,17 @@ function App() {
         localStorage.removeItem('counterValue+1')
         setValue(0)
     }
-  return (
-      <div className="App">
-          <h1>{value}</h1>
-          <button onClick={incHandler}>+</button>
-          <button onClick={decHandler}>-</button>
-          <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-          <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-          <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-          <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
-      </div>
-  )
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>+</button>
+            <button onClick={decHandler}>-</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+        </div>
+    )
 }
 
 export default App
